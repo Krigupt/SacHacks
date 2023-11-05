33bffe3d613212eba5a75407f3e18690ec922b71
Krigupt <123670605+Krigupt@users.noreply.github.com>
2023-11-04 21:41:35 -0700

    update full app

---
+++ Sac/__init__.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "KRISHNA"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:kv0309//@localhost/Sac'
+##THIS IS A COMMAND FOR SQL to connext with XAMPP
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/fashion_classifier'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'krishnaguptapyrax4@gmail.com'
+app.config['MAIL_PASSWORD'] = 'wjgzztvfjbtegtcn'
+
+mail = Mail(app)
+
+
+
+from Sac import route
+
---
+++ Sac/models.py
@@ -0,0 +1,44 @@
+from sqlalchemy import LargeBinary
+from datetime import datetime
+from Sac import db,login_manager,app
+from flask_login import UserMixin
+
+
+
+
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+
+
+class User(db.Model,UserMixin):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    date = db.Column(db.Date)
+    image_file = db.Column(db.String(1000), nullable=False, default='default.png')
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    
+
+   
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}',)"
+
+
+class Post(db.Model):
+    __tablename__ = 'Post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
