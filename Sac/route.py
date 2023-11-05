8a0e36b45335b4aced6099068ad25e08ec78a49a
Krigupt <123670605+Krigupt@users.noreply.github.com>
2023-11-04 22:36:39 -0700

    integrate blog functionality

--- Sac/route.py
+++ Sac/route.py
@@ -253,3 +253,8 @@ def disconnect():
     
     send({"name": name, "message": "has left the room"}, to=room)
     print(f"{name} has left the room {room}")
+
+@app.route("/blog", methods=['POST', 'GET'])
+def blog():
+    posts = Post.query.all()
+    return render_template('blog.html',posts=posts)
