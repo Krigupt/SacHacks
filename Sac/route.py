dead87872306870d15213c55ff672e9495abfcb2
Krigupt <123670605+Krigupt@users.noreply.github.com>
2023-11-05 02:03:38 -0800

    integrate machine learning model into updated version

--- Sac/route.py
+++ Sac/route.py
@@ -1,6 +1,9 @@
 import flask
 from flask import render_template, url_for, flash, redirect,request,abort, session
 import os
+
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
 from Sac.forms import RegistrationForm, LoginForm,UpdateAccountForm,PostForm
 from Sac.models import User,Post
 from flask_login import login_user,current_user,logout_user,login_required
@@ -18,8 +21,7 @@ from string import ascii_uppercase
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
-    return render_template('home.html',posts=posts)
+    return render_template('home.html')
 
 
 @login_required
@@ -258,3 +260,62 @@ def disconnect():
 def blog():
     posts = Post.query.all()
     return render_template('blog.html',posts=posts)
+
+@app.route("/stress")
+def stress():
+    return render_template('stress-test.html', title='Stress')
+
+@app.route("/questionaire", methods=['POST', 'GET'])
+def questionaire():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        gender = request.form.get('gender')
+        occupation = request.form.get('occupation')
+
+        qtags = ['Q' + str(x) for x in range(1, 26)]
+
+        results = [name, gender, occupation]
+
+        for tag in qtags:
+            results.append(request.form.get(tag))
+        
+        print(results)
+        
+        df = pd.read_csv('data-final.csv', sep='\t') 
+        questions = pd.DataFrame(df, columns=['EXT1', 'EXT2', 'EXT3', 'EXT4', 'EXT5', 'EXT6', 'EXT7', 'EXT8', 'EXT9',
+       'EXT10', 'EST1', 'EST2', 'EST3', 'EST4', 'EST5', 'EST6', 'EST7', 'EST8',
+       'EST9', 'EST10', 'AGR1', 'AGR2', 'AGR3', 'AGR4', 'AGR5'])
+        
+        for i in questions.columns:
+            questions[i].fillna(questions[i].mean(), inplace=True)
+
+        temp = []
+        for i in results[3:]:
+            if not i:
+                temp.append(2.5)
+            else:
+                temp.append(i)
+            
+        print(temp)
+
+        nbrs = NearestNeighbors(n_neighbors=5, algorithm='ball_tree').fit(questions)
+
+        input_data = pd.DataFrame([temp], columns=['EXT1', 'EXT2', 'EXT3', 'EXT4', 'EXT5', 'EXT6', 'EXT7', 'EXT8', 'EXT9',
+       'EXT10', 'EST1', 'EST2', 'EST3', 'EST4', 'EST5', 'EST6', 'EST7', 'EST8',
+       'EST9', 'EST10', 'AGR1', 'AGR2', 'AGR3', 'AGR4', 'AGR5'])
+
+        distance, indices = nbrs.kneighbors(input_data)
+
+        print([x for x in indices[0]])
+
+        return redirect(url_for('profiles',))
+    
+    return render_template('questionaire.html', title='Questionaire')
+
+@app.route("/profiles", methods=['GET', 'POST'])
+def profiles():
+    print(request.method)
+    form = RegistrationForm()
+    return render_template('profiles.html', title='Profiles', form=form)
+
+
--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from Sac import app, db, socketio
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True, port=8000)
+    socketio.run(app, debug=True, port=5300)
